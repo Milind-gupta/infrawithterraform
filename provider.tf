529a7d4ad3642390106e76df2d62a58eda9469b9
nitin809 <55194700+nitin809@users.noreply.github.com>
2021-08-01 00:16:53 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA4DLWACUGPRAPJBVP"
+  secret_key = "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"
+}
---
+++ securitygroup.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "pub-sg" {
+  name   = "public-sg"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "pri-sg" {
+  name   = "private-wordpress-sg"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.pub-sg.id]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
